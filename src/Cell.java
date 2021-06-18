ccee6d7703dee8d9219e92fe3cb83038ed028f57
ash-p98 <32458304+ash-p98@users.noreply.github.com>
2021-06-18 16:56:37 +0100

    more notes

--- src/Cell.java
+++ src/Cell.java
@@ -2,5 +2,22 @@ public class Cell {
 
     public static void cellClicked(){
 
+        //3 bools
+        /*
+        * has it been clicked
+        * has a bomb
+        * */
+
+        boolean isRevealed = true;
+
+
+        //place bombs when board created
+
+        // need to show if cell has been revealed use bool?
+
+        // Once revealed check value
+
+        //value based of bomb.
+
     }
 }
